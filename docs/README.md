869c71c00740341dd9162c56feca79f90a672b47
Derek Lee <derek.scott.lee13@gmail.com>
2022-08-02 14:19:51 -0700

    Remove almost everything

--- docs/README.md
+++
@@ -1,9 +0,0 @@
-# No Chat Reports
-
-**Current Version:** 1.19.1-v1.8.2
-
-**Minecraft Version:** 1.19.1
-
-Strips cryptographic signatures from player messages, making it impossible to track and associate them with your Mojang/Microsoft account, as well as use Player Chat Reporting feature.
-
-**GitLab Mirror:** https://gitlab.com/Aizistral-Studios/No-Chat-Reports
--- src/main/java/xyz/holocons/mc/nounsecuretoast/mixin/ToastComponentMixin.java
+++ src/main/java/xyz/holocons/mc/nounsecuretoast/mixin/ToastComponentMixin.java
@@ -1,11 +1,9 @@
-package com.aizistral.nochatreports.mixins.client;
+package xyz.holocons.mc.nounsecuretoast.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.aizistral.nochatreports.core.NoReportsConfig;
 
 import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.client.gui.components.toasts.SystemToast.SystemToastIds;
@@ -13,14 +11,12 @@ import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
 
 @Mixin(ToastComponent.class)
-public class MixinToastComponent {
+public class ToastComponentMixin {
 
 	@Inject(method = "addToast", at = @At("HEAD"), cancellable = true)
 	private void onAddToast(Toast toast, CallbackInfo info) {
-		if (NoReportsConfig.hideWarningToast())
-			if (toast instanceof SystemToast sys && sys.getToken() == SystemToastIds.UNSECURE_SERVER_WARNING) {
-				info.cancel();
-			}
+		if (toast instanceof SystemToast sys && sys.getToken() == SystemToastIds.UNSECURE_SERVER_WARNING) {
+			info.cancel();
+		}
 	}
-
 }
